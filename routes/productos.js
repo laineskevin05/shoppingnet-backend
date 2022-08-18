eb7d74272c5832ce3e1f0e8ce2257c1f649dba60
Kevin Darel Laines <lainezkevin05@gmail.com>
2022-08-18 00:14:59 -0600

    Update productos.js

--- routes/productos.js
+++ routes/productos.js
@@ -1,20 +1,20 @@
-const upload  = require('../helpers/multer')
-const { Router } = require('express')
-const flieUpload = require('express-fileupload')
+const upload = require("../helpers/multer");
+const { Router } = require("express");
+const flieUpload = require("express-fileupload");
 const router = Router();
 
-const productoCtrl = require('../controllers/producto.controller');
-const fileUpload = require('express-fileupload');
+const productoCtrl = require("../controllers/producto.controller");
+const fileUpload = require("express-fileupload");
 const { validarJWT } = require("../middlewares/validar-jwt");
 
-router.get('/',validarJWT, productoCtrl.getProductos)
+router.get("/", productoCtrl.getProductos);
 /* router.post('/', fileUpload({
     useTempFiles: true,
     tempFileDir: "./uploads",
     }), productoCtrl.crearProducto) */
-router.post('/',   productoCtrl.crearProducto)
-router.get('/:id', productoCtrl.getProducto)
-router.put('/:id', productoCtrl.editarProducto)
-router.delete('/:id',productoCtrl.borrarProducto)
+router.post("/", productoCtrl.crearProducto);
+router.get("/:id", productoCtrl.getProducto);
+router.put("/:id", productoCtrl.editarProducto);
+router.delete("/:id", productoCtrl.borrarProducto);
 
-module.exports = router 
+module.exports = router;
